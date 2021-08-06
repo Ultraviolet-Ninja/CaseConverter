314002624c2025807dd8ee4ab70a58eb097222e0
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-08-05 19:52:34 -0500

    Added stuff

--- src/main/java/jasmine/jragon/converter/CaseConverter.java
+++ src/main/java/jasmine/jragon/converter/CaseConverter.java
@@ -1,4 +1,20 @@
 package jasmine.jragon.converter;
 
+import jasmine.jragon.tool.regex.Regex;
+
 public class CaseConverter {
+    private static final Regex SPACE_DELIMITED_REGEX, SCREAMING_KEBAB_CASE_REGEX, SCREAMING_SNAKE_CASE_REGEX,
+            SNAKE_CASE_REGEX, KEBAB_CASE_REGEX, CAMEL_CASE, PASCAL_CASE_REGEX;
+
+    static {
+        SCREAMING_KEBAB_CASE_REGEX = new Regex("\\b([A-Z]+)-?\\b");
+        SCREAMING_SNAKE_CASE_REGEX = new Regex("\\b([A-Z]+)_?\\b");
+        SPACE_DELIMITED_REGEX = new Regex("\\b([a-zA-Z0-9]+) ?\\b");
+        SNAKE_CASE_REGEX = new Regex("");
+        KEBAB_CASE_REGEX = new Regex("");
+        CAMEL_CASE = new Regex("");
+        PASCAL_CASE_REGEX = new Regex("");
+    }
+
+
 }
